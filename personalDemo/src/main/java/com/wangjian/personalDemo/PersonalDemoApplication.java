3cefd276aa8a431e73bcc7808454b6f10a0fafe6
wangjian <65283127@qq.com>
2018-09-03 17:26:14 +0800

    first commit

---
+++ personalDemo/src/main/java/com/wangjian/personalDemo/PersonalDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wangjian.personalDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDemoApplication.class, args);
+	}
+}
